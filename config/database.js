cc2897dc315c4fccc54f2816245993cbbb1d3050
Anthony Stolp <anthony.stolp@gmail.com>
2024-08-13 08:25:43 -0400

    trying to resolve sq lite and postgre sissue

--- config/database.js
+++ config/database.js
@@ -1,26 +1,13 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
+  const client = env("DATABASE_CLIENT", "postgres");
 
   const connections = {
     postgres: {
       connection: {
-        connectionString: env(
-          "DATABASE_PUBLIC_URL",
-          `postgres://strapi:strapi@localhost:5432/strapi`
-        ),
-        host: env("PGHOST", "localhost"),
-        port: env.int("PGPORT", 5432),
-        database: env("POSTGRES_DB", "strapi"),
-        user: env("PGUSER", "strapi"),
-        password: env("POSTGRES_PASSWORD", "strapi"),
+        connectionString: env("DATABASE_URL"),
         ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
             true
