fafe5b9e3ab121f5bcb913c97ca736eb69143395
Anthony Stolp <anthony.stolp@gmail.com>
2024-08-13 08:55:55 -0400

    fixed database.js file

--- config/database.js
+++ config/database.js
@@ -1,24 +1,39 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
+  const client = env("DATABASE_CLIENT", "sqlite");
+
+  const isProduction = env("NODE_ENV") === "production";
 
   const connections = {
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
+        connectionString: isProduction
+          ? env("DATABASE_URL") // Internal URL for production
+          : env("DATABASE_PUBLIC_URL", "DATABASE_LOCAL_URL"), // Public URL for local development
+        ssl: isProduction &&
+          env.bool("DATABASE_SSL", false) && {
+            rejectUnauthorized: env.bool(
+              "DATABASE_SSL_REJECT_UNAUTHORIZED",
+              true
+            ),
+          },
         schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
       },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
   };
 
