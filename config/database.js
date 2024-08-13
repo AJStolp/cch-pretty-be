db13692ac4e93f7cce57bd44c28c4e1796193c27
Anthony Stolp <anthony.stolp@gmail.com>
2024-08-13 10:45:22 -0400

    another database..js config adjustment

--- config/database.js
+++ config/database.js
@@ -1,12 +1,9 @@
-const path = require("path");
-
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
   const isProduction = env("NODE_ENV") === "production";
 
-  const connections = {
-    postgres: {
+  return {
+    connection: {
+      client: "postgres",
       connection: {
         connectionString: isProduction
           ? env("DATABASE_URL") // Internal URL for production
@@ -24,13 +21,6 @@ module.exports = ({ env }) => {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
       },
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
   };
